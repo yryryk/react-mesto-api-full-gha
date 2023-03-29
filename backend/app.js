37a699559240198a3ee6524cd72f49552813b715
yryryk <tovchennikov@e1.ru>
2023-03-29 23:51:47 +0500

    feat: добавить crash-test

--- backend/app.js
+++ backend/app.js
@@ -45,6 +45,12 @@ app.use((req, res, next) => {
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
